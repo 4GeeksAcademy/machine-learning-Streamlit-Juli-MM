e0f5b8aa1c3d3548df50b69ea76bf797bf97a2ec
Juliana Moitinho Meira <157488915+Juli-MM@users.noreply.github.com>
2024-06-29 20:02:17 +0000

    Loading

--- src/app.py
+++ src/app.py
@@ -1,4 +1,38 @@
-from utils import db_connect
-engine = db_connect()
+import os
+import pickle
+import streamlit as st
 
-# your code here
+#Load the model
+with open('/workspaces/machine-learning-Streamlit-Juli-MM/models/tree_classifier_crit-gini_maxdepth-5_minleaf-2_minsplit-10_42.sav', 'rb') as file:
+    model = pickle.load(file)
+
+# Streamlit app
+def main():
+    st.title("Diabetes Predictor")
+    
+    st.write("This predictor uses a Decision Tree optimized model with a 80% accuracy in predicting, based on diagnostic measures, whether or not a patient has diabetes.")
+    st.write("Please fill in the blanks with your information:")
+    
+    number1 = st.number_input("Pregnancies", value=0, min_value=0, step=1)
+    st.divider()
+    number2 = st.number_input("Glucose", value=0, min_value=0, step=1)
+    st.divider()
+    number3 = st.number_input("BloodPressure", value=0, min_value=0, step=1)
+    st.divider()
+    number4 = st.number_input("BMI", value=0.0, min_value=0.0, format="%.2f")
+    st.divider()
+    number5 = st.number_input("Diabetes Pedigree Function", value=0.0, min_value=0.0, format="%.3f")
+    st.divider()
+    number6 = st.number_input("Age", value=0, min_value=0, step=1)
+    
+    if st.button("Predict"):
+        try:
+            prediction = model.predict([[number1, number2, number3, number4, number5, number6]])[0]
+            class_dict = {0: "Non diabetic", 1: "Diabetic"}
+            pred_class = class_dict[prediction]
+            st.write("Prediction:", pred_class)
+        except Exception as e:
+            st.error(f"An error occurred during prediction: {e}")
+
+if __name__ == '__main__':
+    main()
